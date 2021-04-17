c35b1fb18b0d04649233e198f87bfc8fcf9058ab
sansona <jiaming.justin.chen@gmail.com>
2021-04-17 07:32:17 -0700

    Fixed bug with get_population_at_each_generation tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -14,8 +14,10 @@ def dummy_population() -> np.ndarray:
     """
     return np.array([[BaseBlob(0.5, 1.0, 1.0),
             BaseBlob(0.5, 1.0, 1.0),
+            MutatedBlob(0.3, 1.0, 1.0),
             MutatedBlob(0.3, 1.0, 1.0)],
-            [BaseBlob(0.5, 0.5, 0.5)]])
+            [BaseBlob(0.5, 0.5, 0.5)]],
+            dtype=object)
 
 @pytest.fixture
 def dummy_attributes() -> np.ndarray:
@@ -26,18 +28,21 @@ def dummy_attributes() -> np.ndarray:
     Returns:
         (np.ndarray)
     """
-    return np.array([0.01, 0.01, 0.01])
+    return np.array([0.01, 0.01, 0.01, 0.01])
 
 def test_get_pivot_indices_parses_correct_blob_types(dummy_population):
     """Tests that get_pivot_indices gets correct blob types"""
     indices = get_pivot_indices(dummy_population[0])
-    assert list(indices[0]) == ['BaseBlob', 'BaseBlob', 'MutatedBlob']
+    assert list(indices[0]) == ['BaseBlob',
+                                'BaseBlob',
+                                'MutatedBlob',
+                                'MutatedBlob']
 
 def test_get_pivot_indices_splits_correct_indices(dummy_population):
     """Tests that get_pivot_indices outputs correct indices of blob type
     changes"""
     indices = get_pivot_indices(dummy_population[0])
-    assert list(indices[1]) == [1, 2]
+    assert list(indices[1]) == [1, 3]
 
 def test_apply_mask_properly_applies_mask(dummy_population, dummy_attributes):
     """Tests that apply_mask_to_population properly masks population on
@@ -50,14 +55,14 @@ def test_get_generation_attributes(dummy_population):
     """Tests that get_generation_attributes retrieves correct attribute values
     """
     attrs = get_generation_attributes(dummy_population[0], 'survival_prob')
-    assert list(attrs) == [0.5, 0.5, 0.3]
+    assert list(attrs) == [0.5, 0.5, 0.3, 0.3]
 
 def test_get_population_at_each_gen_retrieves_proper_counts(dummy_population):
     """Tests that get_population_at_each_generation retrieves proper count
     of blob types across entire population"""
     type_counts, _ = get_population_at_each_generation(dummy_population)
     assert type_counts == {'BaseBlob': [2, 1],
-                        'MutatedBlob': [1, 0]}
+                        'MutatedBlob': [2, 0]}
 
 def test_get_population_at_each_gen_retrieves_proper_colors(dummy_population):
     """Tests that get_population_at_each_generation retrieves proper color
